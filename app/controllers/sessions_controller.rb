2111255343abd623f27533f2937a1e23120eed13
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-07-18 13:49:55 +0200

    modification du controller pour l'authentification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
+require 'jwt'
+
 class SessionsController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      payload = { user_id: user.id, exp: 24.hours.from_now.to_i }
-      token = Authentication::JwtService.encode(payload)
-      render json: { token: token }
-    else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    def create
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        payload = { user_id: user.id, exp: 24.hours.from_now.to_i }
+        token = generate_jwt(user) # Fonction pour générer un JWT avec l'utilisateur
+        render json: { user: user, token: token }
+      else
+        render json: { error: 'Email ou mot de passe incorrect' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def generate_jwt(user)
+      JWT.encode({ user_id: user.id }, Rails.application.secrets.secret_key_base)
     end
   end
-end
+  
